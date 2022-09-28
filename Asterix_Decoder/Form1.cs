d7c9a52a7eb80df1ebe950f1448df665ff7cf592
Pablo Garcia <pablo.garcia.mediavilla@office365.estudiantat.upc.edu>
2022-09-28 19:04:24 +0200

    Agregar archivos de proyecto.

---
+++ Asterix_Decoder/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Class_Library;
+
+
+namespace Asterix_Decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialogAsterix.InitialDirectory = "c:\\";
+            openFileDialogAsterix.Filter = "ast files (*.ast)|*.ast|All files (*.*)|*.*";
+            openFileDialogAsterix.FilterIndex = 2;
+            openFileDialogAsterix.RestoreDirectory = true;
+
+            if (openFileDialogAsterix.ShowDialog() == DialogResult.OK)
+            {
+                Class_Library.Asterix_File Asterix = new Class_Library.Asterix_File(openFileDialogAsterix.FileName);
+                if (Asterix.path != null)
+                {
+                    textBox1.Text = Asterix.path;
+                }
+            }
+        }
+    }
+}
---
+++ Class_Library/Asterix_File.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Class_Library
+{
+    public class Asterix_File
+    {
+        public string path = null;
+
+        //public List<CAT10> listCAT10 = new List<CAT10>();
+
+        //public List<CAT21> listCAT21 = new List<CAT21>();
+
+        DataTable tabla_CAT10 = new DataTable();
+
+        DataTable tabla_CAT21 = new DataTable();
+
+        public Asterix_File(string path)
+        {
+            this.path = path;
+        }
+    }
+}
